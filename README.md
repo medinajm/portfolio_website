a4e3415abcf962a1b6374d95f506bc0db2fc2fa8
medinajm <104926468+medinajm@users.noreply.github.com>
2022-08-21 17:12:26 -0400

    changed button code and added text

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Jeff Medina's Portfolio Website
-## More to come
+## Codecademey Project
+
+For this project I was tasked with using my knowledge of html, css and javascript to create a simple website that gives a brief bio and links to some projects that I've done so far. 
--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// let intro = document.getElementsByClassName('intro')
-
-// function changeTextColor {
-//     intro.style.color = 'red';
-// }
-// intro.addEventListener('mouseover', changeTextColor)
-
+// JS for index.html
+let origName = document.querySelector('h1').innerText
 let h1Name = document.querySelector("h1");
 function changeText() {
     h1Name.innerHTML = "My name is Jeff!";
@@ -12,11 +7,13 @@ function changeText() {
 }
 function changeBack() {
     h1Name.style.color = ''
-    h1Name.innerHTML = 'Jeff Medina'
+    h1Name.innerHTML = origName
 }
 h1Name.addEventListener("mouseover", changeText);
 h1Name.addEventListener('mouseout', changeBack);
 
+
+// JS for contact.html
 let mailImage = document.getElementById('mail');
 let rotation = 0
 function rotateMail() {
@@ -27,3 +24,22 @@ function rotateMail() {
     mailImage.style.transform = `rotate(${rotation}deg)`
 }
 let startRotate = window.setInterval(rotateMail, 1000);
+
+//script for projects page
+// Creates a random number between 0 and 4.
+const randNum = Math.floor(Math.random() * 5)
+
+const msgCont = {
+    today:[["finish that project"], ["take the next step"], ["reach the next level"], ["try something new"], ["make a new memory"]],
+    walk: [["kindness is always the best option"], ["tomorrow is never promised"], ["life is a gift"], ["love is life's greatest gifts"], ["life is about experiences"]],
+    remember: [["are awesome"], ["are great"], ["can do anything"], ["can choose to be better"], ["are never too old to learn and grow"]],
+}
+
+const msgOut = () => {
+    let today = (`Today is the day that you'll ${msgCont.today[randNum]}`);
+    let walk = (`Take a walk and remind yourself that ${msgCont.walk[randNum]}`);
+    let remember = (`Always remember that you ${msgCont.remember[randNum]}`);
+    document.getElementById('demo1').innerHTML = today;
+    document.getElementById('demo2').innerHTML = walk;
+    document.getElementById('demo3').innerHTML = remember;
+}
